e6df61394d408f6ad90fc52df19c6ceaa1f527fc
Taylor Burris <taylorcburris@gmail.com>
2026-04-09 17:02:19 -0500

    ANSI UI upgrade: dashboard styling, trend arrows, egg production bars

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,36 @@
 #include <iostream>
+#ifdef _WIN32
+#include <windows.h>
+#endif
 #include "CoopTracker.h"
 
 using namespace std;
 
 int main() {
+#ifdef _WIN32
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (hOut != INVALID_HANDLE_VALUE) {
+        DWORD dwMode = 0;
+        if (GetConsoleMode(hOut, &dwMode)) {
+            SetConsoleMode(hOut, dwMode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+        }
+    }
+#endif
+
     CoopTracker tracker;
 
-    cout << "        ,~.\n";
-    cout << "       ('v')\n";
-    cout << "      ((___))\n";
-    cout << "       ^   ^\n";
-    cout << "============================================================\n";
-    cout << "                     COOPKEEPER CLI\n";
-    cout << "              Chicken Coop Management System\n";
-    cout << "============================================================\n\n";
-    cout << "Track chickens, eggs, feed, expenses, health, and cleaning.\n";
+    cout << "\033[35m        ,~.\033[0m\n";
+    cout << "\033[35m       ('v')\033[0m\n";
+    cout << "\033[35m      ((___))\033[0m\n";
+    cout << "\033[35m       ^   ^\033[0m\n";
+    cout << "\033[90m============================================================\033[0m\n";
+    cout << "\033[1;96m                     COOPKEEPER CLI\033[0m\n";
+    cout << "\033[1;36m              Chicken Coop Management System\033[0m\n";
+    cout << "\033[90m============================================================\033[0m\n\n";
+    cout << "\033[2mTrack chickens, eggs, feed, expenses, health, and cleaning.\033[0m\n";
 
     tracker.run();
 
-    cout << "\nThanks for using CoopKeeper!\n";
+    cout << "\n\033[1;32mThanks for using CoopKeeper!\033[0m\n";
     return 0;
 }
